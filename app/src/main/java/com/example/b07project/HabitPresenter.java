9f37e5ae4ded09eba509a4980aae500797bfac24
Josh <eeei3.joshua0307lol@gmail.com>
2024-11-22 21:57:52 -0500

    feat: wrote function declarations for all methods required for Habit Presenter.

--- app/src/main/java/com/example/b07project/HabitPresenter.java
+++ app/src/main/java/com/example/b07project/HabitPresenter.java
@@ -8,4 +8,32 @@ public class HabitPresenter {
         this.userid = userid;
         presenterToModel = GeneralServerCommunicator.createInstance(userid);
     }
+
+    public void searchByName(String filter) {
+
+    }
+
+    public void searchByCategory(String filter) {
+
+    }
+
+    public void searchByImpact(String filter) {
+
+    }
+
+    public void userAddGoal(String goal) {
+
+    }
+
+    public void userGetGoal(String goal) {
+
+    }
+
+    public void userSetProg(String goal) {
+
+    }
+
+    public void userGetProg(String goal) {
+
+    }
 }
