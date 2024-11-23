fe091775af0aef7b5acd8efdf22baf7703d52965
Monica Vergara <monicavergara@Monicas-MacBook-Air.local>
2024-11-23 13:59:50 -0500

    added back-end user registration functionality

---
+++ app/src/main/java/com/example/registration/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.registration;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.util.Log;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+//entry point of app
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    FirebaseDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+//        db = FirebaseDatabase.getInstance("https://registration-d6a6c-default-rtdb.firebaseio.com/");
+//        DatabaseReference myRef = db.getReference("test");
+//
+//        //myRef.setValue("Registration Demo!");
+//        myRef.child("movies").setValue("B07 Demo!");
+
+        //checks if activity is being started for the first time
+        if (savedInstanceState == null) {
+            loadFragment(new Home()); //adds homefragment to the activity
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
+            getSupportFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
