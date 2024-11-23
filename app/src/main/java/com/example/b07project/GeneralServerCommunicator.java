e97b9ebde9d0021462215508e6a8bf3859f448a5
Josh <eeei3.joshua0307lol@gmail.com>
2024-11-22 21:57:27 -0500

    feat: wrote a prototype for all habits Model methods, and modified SuccessListener to reflect new needs.

--- app/src/main/java/com/example/b07project/GeneralServerCommunicator.java
+++ app/src/main/java/com/example/b07project/GeneralServerCommunicator.java
@@ -1,12 +1,37 @@
 package com.example.b07project;
 
+import java.util.HashSet;
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.ValueEventListener;
+
+class Goal {
+    String name;
+    int prog;
+    HashSet<String> types;
+
+    public Goal(String name) {
+        this.name = name;
+        this.prog = 0;
+        this.types = new HashSet<String>();
+    }
+
+    public Goal(String name, int prog) {
+        this.name = name;
+        this.prog = prog;
+        this.types = new HashSet<String>();
+    }
+}
+
+
 /**
  * How the Database will work
  * Habitslist - (this is a list of habits the user can choose to pick up)
@@ -24,6 +49,7 @@ import com.google.firebase.database.FirebaseDatabase;
  *              - Don't use the jetpack
  *                  - 20%
  */
+
 final public class GeneralServerCommunicator {
     private static GeneralServerCommunicator serverCommunicator;
     private FirebaseDatabase db;
@@ -54,17 +80,23 @@ final public class GeneralServerCommunicator {
      * Get the list of goals available on the Database
      */
     void getListGoals(SuccessListener watcher) {
-        dbworker.child("habitslist").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        Query listgoals = dbworker.child("habitslist");
+        listgoals.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (task.isSuccessful()) {
-                    // Fetch the values we want
-
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                HashSet<String> res = new HashSet<String>();
+                for (DataSnapshot goals: snapshot.getChildren()) {
+                    res.add(goals.getKey());
                 }
-                else {
-                    // Return an error
+                watcher.setSuccess(true);
+                watcher.setListgoals(res);
+                listener.onObjectReady(watcher);
+            }
 
-                }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                watcher.setSuccess(false);
+                listener.onObjectReady(watcher);
             }
         });
     }
@@ -72,19 +104,32 @@ final public class GeneralServerCommunicator {
     /**
      * Get the list of goals the user is currently working on
      */
-    void getGoals(SuccessListener watcher) {
-        dbworker.child("userhabits").child(userid).child("habits").get()
-                .addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+    void getGoals(String userid, SuccessListener watcher) {
+        Query usergoals = dbworker.child("users").child(userid);
+        usergoals.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (task.isSuccessful()) {
-                    // Fetch the values we want
-
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                HashSet<Goal> res = new HashSet<Goal>();
+                for (DataSnapshot goals: snapshot.getChildren()) {
+                    try {
+                        int prog = goals.getValue(Integer.class);
+                        Goal temp = new Goal(goals.getKey(), prog);
+                        res.add(temp);
+                    }
+                    catch (NullPointerException ex) {
+                        Goal temp = new Goal(goals.getKey());
+                        res.add(temp);
+                    }
                 }
-                else {
-                    // Return an error
+                watcher.setSuccess(true);
+                watcher.setUsergoals(res);
+                listener.onObjectReady(watcher);
+            }
 
-                }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                watcher.setSuccess(false);
+                listener.onObjectReady(watcher);
             }
         });
     }
@@ -92,60 +137,62 @@ final public class GeneralServerCommunicator {
     /**
      * Set a new goal for the user
      */
-    void setGoals(SuccessListener watcher) {
-//        dbworker.child("habitslist").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DataSnapshot> task) {
-//                if (task.isSuccessful()) {
-//                    // Fetch the values we want
-//
-//                }
-//                else {
-//                    // Return an error
-//
-//                }
-//            }
-//        });
+    void setGoals(String goalname, SuccessListener watcher) {
+        dbworker.child("users").child(userid).setValue(new Goal(goalname))
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            watcher.setSuccess(true);
+                            listener.onObjectReady(watcher);
+                        }
+                        else {
+                            listener.onObjectReady(watcher);
+                        }
+                    }
+                });
     }
 
     /**
      * Get the progress of a goal that the user is working on
      */
     void getProg(String goal, SuccessListener watcher) {
-        dbworker.child("userhabits").child(userid).child("habits").child(goal)
+        dbworker.child("users").child(userid).child("habits").child(goal)
                 .get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (task.isSuccessful()) {
-                    // Fetch the values we want
-
-                }
-                else {
-                    // Return an error
-
-                }
-            }
-        });
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            // Fetch the values we want
+                            watcher.value = Integer.parseInt((String) task.getResult().getValue());
+                            watcher.setSuccess(true);
+                            listener.onObjectReady(watcher);
+                        }
+                        else {
+                            // Return an error
+                            watcher.setSuccess(false);
+                            listener.onObjectReady(watcher);
+                        }
+                    }
+                });
     }
 
     /**
      * Set the progress of a goal that the user is working on
      */
-    void setProg(String goal, SuccessListener watcher) {
-//        dbworker.child("userhabits").child(userid).child("habits").child(goal)
-//                .get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DataSnapshot> task) {
-//                if (task.isSuccessful()) {
-//                    // Fetch the values we want
-//
-//                }
-//                else {
-//                    // Return an error
-//
-//                }
-//            }
-//        });
+    void setProg(String goalname, int prog, SuccessListener watcher) {
+        dbworker.child("users").child(userid).child(goalname).setValue(prog)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            watcher.setSuccess(true);
+                            listener.onObjectReady(watcher);
+                        }
+                        else {
+                            listener.onObjectReady(watcher);
+                        }
+                    }
+                });
     }
 
     public static GeneralServerCommunicator createInstance(String userid) {
--- app/src/main/java/com/example/b07project/SuccessListener.java
+++ app/src/main/java/com/example/b07project/SuccessListener.java
@@ -1,9 +1,13 @@
 package com.example.b07project;
 
+import java.util.HashSet;
+
 // We use this object to simply keep track of success or failure
 public class SuccessListener {
     boolean success;
     int value;
+    HashSet<Goal> usergoals;
+    HashSet<String> listgoals;
 
     public SuccessListener() {
         this.success = false;
@@ -16,4 +20,7 @@ public class SuccessListener {
     public void setValue(int value) {
         this.value = value;
     }
+
+    public void setUsergoals(HashSet<Goal> value) {this.usergoals = value;}
+    public void setListgoals(HashSet<String> value) {this.listgoals = value;}
 }
