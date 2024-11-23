c82638df41c18bc446c37f2ec2c4f3746a4ff210
Josh <eeei3.joshua0307lol@gmail.com>
2024-11-23 00:00:57 -0500

    feat: wrote the implementation for all of the methods in HabitPresenter and added documentation

--- app/src/main/java/com/example/b07project/HabitPresenter.java
+++ app/src/main/java/com/example/b07project/HabitPresenter.java
@@ -3,37 +3,169 @@ package com.example.b07project;
 public class HabitPresenter {
     GeneralServerCommunicator presenterToModel;
     String userid;
+    HabitPresenter.PresenterViewPipe listener;
+
+    public interface PresenterViewPipe {
+        void onObjectReady(SuccessListener listener);
+    }
 
     public HabitPresenter(String userid) {
         this.userid = userid;
-        presenterToModel = GeneralServerCommunicator.createInstance(userid);
+        this.presenterToModel = GeneralServerCommunicator.createInstance(userid);
     }
 
-    public void searchByName(String filter) {
-
+    /**
+     * setViewPipe permits the View to communicate with the Presenter
+     * @param listener
+     */
+    public void setViewPipe(HabitPresenter.PresenterViewPipe listener) {
+        this.listener = listener;
     }
 
-    public void searchByCategory(String filter) {
-
+    /**
+     * searchByName lets the user search potential goals by it's name
+     * @param filter - The filter the user wishes to apply to the list
+     * @param pv - How we notify the View that the results are ready
+     */
+    public void searchByName(String filter, SuccessListener pv) {
+        // mp is the listener that we use to tell if the Model operation succeeded
+        SuccessListener mp = new SuccessListener();
+        this.presenterToModel.setModelPipe(new ServerCommunicator.ModelPresenterPipe() {
+            @Override
+            public void onObjectReady(SuccessListener betweener) {
+                pv.setSuccess(true);
+                for (Goal g: betweener.usergoals) {
+                    pv.listgoals.add(g.name);
+                }
+                listener.onObjectReady(pv);
+            }
+        });
+        this.presenterToModel.getListGoals(mp);
     }
 
-    public void searchByImpact(String filter) {
+    /**
+     * searchByCategory lets the user search potential goals by it's category
+     * @param filter - The filter the user wishes to apply to the list
+     * @param pv - How we notify the View that the results are ready
+     */
+    public void searchByCategory(String filter, SuccessListener pv) {
+        // mp is the listener that we use to tell if the Model operation succeeded
+        SuccessListener mp = new SuccessListener();
+        this.presenterToModel.setModelPipe(new ServerCommunicator.ModelPresenterPipe() {
+            @Override
+            public void onObjectReady(SuccessListener betweener) {
+                pv.setSuccess(true);
+                for (Goal g: betweener.usergoals) {
+                    pv.listgoals.add(g.name);
 
+                }
+                listener.onObjectReady(pv);
+            }
+        });
+        this.presenterToModel.getListGoals(mp);
     }
 
-    public void userAddGoal(String goal) {
-
+    /**
+     * searchByImpact lets the user search potential goals by it's impact
+     * @param filter - The filter the user wishes to apply to the list
+     * @param pv - How we notify the View that the results are ready
+     */
+    public void searchByImpact(String filter, SuccessListener pv) {
+        // mp is the listener that we use to tell if the Model operation succeeded
+        SuccessListener mp = new SuccessListener();
+        this.presenterToModel.setModelPipe(new ServerCommunicator.ModelPresenterPipe() {
+            @Override
+            public void onObjectReady(SuccessListener betweener) {
+                pv.setSuccess(true);
+                for (Goal g: betweener.usergoals) {
+                    pv.listgoals.add(g.name);
+                }
+                listener.onObjectReady(pv);
+            }
+        });
+        this.presenterToModel.getListGoals(mp);
     }
 
-    public void userGetGoal(String goal) {
-
+    /**
+     * userAddGoal updates the goals that the user has active
+     * @param goal - The goal the user wishes to work towards
+     * @param pv - How we notify the View that the results are ready
+     */
+    public void userAddGoal(String goal, SuccessListener pv) {
+        SuccessListener mp = new SuccessListener();
+        this.presenterToModel.setModelPipe(new ServerCommunicator.ModelPresenterPipe() {
+            @Override
+            public void onObjectReady(SuccessListener betweener) {
+                pv.setSuccess(mp.success);
+                listener.onObjectReady(pv);
+            }
+        });
+        this.presenterToModel.setGoals(goal, mp);
     }
 
-    public void userSetProg(String goal) {
+    /**
+     * userGetGoal gets the list of goals that the user has active
+     * @param filter - If the user chooses to filter the results
+     * @param pv - How we notify the View that the results are ready
+     */
+    public void userGetGoal(String filter, SuccessListener pv) {
+        SuccessListener mp = new SuccessListener();
+        this.presenterToModel.setModelPipe(new ServerCommunicator.ModelPresenterPipe() {
+            @Override
+            public void onObjectReady(SuccessListener betweener) {
+                if (mp.success) {
+                    for (Goal g : mp.usergoals) {
+                        if ((filter != null) || (filter == g.name)) {
+                            pv.usergoals.add(g);
+                        }
+                    }
+                }
+                pv.setSuccess(mp.success);
+                listener.onObjectReady(pv);
 
+            }
+        });
+        this.presenterToModel.getGoals(mp);
     }
 
-    public void userGetProg(String goal) {
+    /**
+     * userSetProg, updates the user's progress towards a goal
+     * @param goal - The goal the user wishes to update
+     * @param prog - The user's new progress
+     * @param pv - How we notify the View that the results are ready
+     */
+    public void userSetProg(String goal, int prog, SuccessListener pv) {
+        SuccessListener mp = new SuccessListener();
+        this.presenterToModel.setModelPipe(new ServerCommunicator.ModelPresenterPipe() {
+            @Override
+            public void onObjectReady(SuccessListener betweener) {
+                pv.setSuccess(mp.success);
+                listener.onObjectReady(pv);
+            }
+        });
+        this.presenterToModel.setProg(goal, prog, mp);
+    }
 
+    /**
+     * userGetProg, fetches the user's progress towards a goal
+     * @param goal - The goal whose progress the user wishes to fetch
+     * @param pv - How we notify the View that the results are ready
+     */
+    public void userGetProg(String goal, SuccessListener pv) {
+        SuccessListener mp = new SuccessListener();
+        this.presenterToModel.setModelPipe(new ServerCommunicator.ModelPresenterPipe() {
+            @Override
+            public void onObjectReady(SuccessListener betweener) {
+                if (mp.success) {
+                    pv.setSuccess(mp.success);
+                    pv.setValue(mp.value);
+                }
+                else {
+                    pv.setSuccess(mp.success);
+                }
+                listener.onObjectReady(pv);
+            }
+        });
+        this.presenterToModel.getProg(goal, mp);
     }
 }
