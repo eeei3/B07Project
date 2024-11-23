094eda2debd6ca151608f29d628feab1c76a0f4a
de3pti <gorrepatideepti@gmail.com>
2024-11-23 13:39:43 -0500

    Implemented Annual Carbonfootprint Survey, Must add values for Q14: Other

---
+++ app/src/main/java/com/example/b07project/User.java
@@ -0,0 +1,12 @@
+package com.example.b07project;
+
+public class User {
+    String name;
+    String username;
+    String email;
+    double transportationEmissions;
+    double foodEmissions;
+    double housingEmissions;
+    double consumptionEmissions;
+    double totalEmissions;
+}
