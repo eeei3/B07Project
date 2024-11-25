6f918d53f2afdab879319a46eecd34db37aa4965
de3pti <gorrepatideepti@gmail.com>
2024-11-25 00:23:25 -0500

    Created an entry page as the starting point of the app

---
+++ app/src/main/java/com/example/b07project/EntryActivity.java
@@ -0,0 +1,18 @@
+package com.example.b07project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EntryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_page);  // Link the XML layout to the activity
+    }
+}
+
+
