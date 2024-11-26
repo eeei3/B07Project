930f53a7fed635bab8326c51a75058b05ae19311
Judy <judy@judys-mbp.home>
2024-11-25 23:10:16 -0500

    feature:eco tracker part A front end

---
+++ app/src/main/java/com/example/b07project/DailyEmissionCalculator.java
@@ -0,0 +1,10 @@
+package com.example.b07project;
+
+public class DailyEmissionCalculator {
+
+    public double getDrivePersonalVehicleEmission(double distance, String vehicleType) {
+        double emissions = 0;
+
+        if()
+    }
+}
