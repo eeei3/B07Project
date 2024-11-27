de40ccaa38fa9b2781c0fc9150552df70217620f
Tommy Nguyen <haidangasian@gmail.com>
2024-11-26 22:21:04 -0500

    chore: added comments / documentation for HabitsAdapter

--- app/src/main/java/com/example/b07project/HabitsAdapter.java
+++ app/src/main/java/com/example/b07project/HabitsAdapter.java
@@ -1,7 +1,6 @@
 package com.example.b07project;
 
 import static android.view.View.GONE;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -18,21 +17,46 @@ import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
+/**
+ * HabitsAdapter class is responsible for the creation and behaviour of the adapter for HabitsModel.
+ *
+ */
 public class HabitsAdapter extends RecyclerView.Adapter<HabitsAdapter.MyViewHolder> {
     Context context;
     ArrayList<HabitsModel> habitsModels;
 
+    /**
+     * Constructor for HabitsAdapter.
+     *
+     * @param context the context to be set as the adapter's field.
+     * @param habitsModels the ArrayList of HabitsModels to be set as the adapter's field.
+     */
     public HabitsAdapter(Context context, ArrayList<HabitsModel> habitsModels) {
         this.context = context;
         this.habitsModels = habitsModels;
     }
 
+    /**
+     * Method to set the field habitsModels and notify the adapter of the dataset changes.
+     *
+     * @param habitsModels the ArrayList of HabitsModels to be set as the adapter's field
+     */
     @SuppressLint("NotifyDataSetChanged")
     public void setHabitsModels(ArrayList<HabitsModel> habitsModels) {
         this.habitsModels = habitsModels;
         notifyDataSetChanged();
     }
 
+    /**
+     * Called when a new ViewHolder is needed to represent an item in the RecyclerView.
+     * This method inflates the item layout and creates a new instance of `MyViewHolder`
+     * to hold the item view.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to
+     *                 an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new instance of `MyViewHolder` which holds the view for an individual habit item.
+     */
     @NonNull
     @Override
     public HabitsAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -41,44 +65,55 @@ public class HabitsAdapter extends RecyclerView.Adapter<HabitsAdapter.MyViewHold
         return new HabitsAdapter.MyViewHolder(view);
     }
 
+    /**
+     * Binds the data of a habit item to the views in the `ViewHolder` at the specified position
+     * in the data set. This method updates the contents of the `ViewHolder` based on the habit's
+     * properties and handles user interactions such as showing or hiding the cancel button and
+     * displaying a confirmation dialog for habit removal.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull HabitsAdapter.MyViewHolder holder, int position) {
+        // set the components for the current habit
         HabitsModel habit = habitsModels.get(position);
         holder.habitName.setText(habit.getHabitName());
         holder.habitImage.setImageResource(habit.getImage());
         holder.habitImpact.setText(habit.getImpact());
+
+        // define default look for certain components
         int white = ContextCompat.getColor(context, R.color.white);
         holder.habitCard.setCardBackgroundColor(white);
-        holder.cancelButton.setVisibility(GONE); //make sure cancel btn is only visible in users page
+        holder.cancelButton.setVisibility(GONE);
 
+        // define behaviour/look of components other than the cancel button (if visible)
         if (!HabitsMenu.currentMenu[0]) {
+            // if on All Habits menu, correctly display adopted habits, if any
             if (HabitsMenu.userHabitsModels.contains(habit)) {
                 int planetzeColour2 = ContextCompat.getColor(context, R.color.planetze_colour_2);
                 holder.habitCard.setCardBackgroundColor(planetzeColour2);
             }
         } else {
+            // if on User's Habits menu, allow option to delete an adopted habit
             holder.cancelButton.setVisibility(View.VISIBLE);
 
+            // if cancel habit is clicked, launch a confirmation dialog
             holder.cancelButton.setOnClickListener(v -> {
                 AlertDialog.Builder builder = new AlertDialog.Builder(context);
                 builder.setMessage("Do you want to remove this habit? All progress will be lost.")
                         .setTitle("Confirm")
                         .setPositiveButton("No", (dialog, id) -> {
-                            // Handle the "No" action
-                            Toast.makeText(context, "Habit Removed", Toast.LENGTH_SHORT).show();
-                            int pos = habitsModels.indexOf(habit); // Get the position of the habit in the list
-                            HabitsMenu.userHabitsModels.remove(habit);
-
-
-                            notifyItemRemoved(pos);  // Notify the adapter to remove the item at the position
+                            // no action is carried out
                         })
                         .setNegativeButton("Yes", (dialog, id) -> {
                             // Handle the "Yes" action
-                            Toast.makeText(context, "Habit Removed", Toast.LENGTH_SHORT).show();
-                            int pos = habitsModels.indexOf(habit); // Get the position of the habit in the list
+                            int pos = habitsModels.indexOf(habit);
                             HabitsMenu.userHabitsModels.remove(habit);
-
-                            notifyItemRemoved(pos);  // Notify the adapter to remove the item at the position
+                            // Notify the adapter to remove the item at the position
+                            notifyItemRemoved(pos);
+                            Toast.makeText(context, "Habit Removed", Toast.LENGTH_SHORT).show();
                         });
                 // Create and show the dialog
                 AlertDialog dialog = builder.create();
@@ -86,8 +121,10 @@ public class HabitsAdapter extends RecyclerView.Adapter<HabitsAdapter.MyViewHold
             });
         }
 
+        // define behaviour if the habit item is clicked
         holder.itemView.setOnClickListener(v -> {
             if (!HabitsMenu.currentMenu[0]) {
+                // if on All Habits menu, launch a dialog to elaborate on the habit's details
                 HabitsDetailsDialogFragment habitDialog = HabitsDetailsDialogFragment.newInstance(
                         habit.getHabitDesc(),
                         habit.getImpactDesc(),
@@ -96,6 +133,7 @@ public class HabitsAdapter extends RecyclerView.Adapter<HabitsAdapter.MyViewHold
                 habitDialog.show(((AppCompatActivity) context).getSupportFragmentManager(),
                         "habit_details_dialog");
             } else {
+                // if on User's Habits menu, launch a dialog to display and log the habit's activities
                 UserHabitsProgressDialogFragment habitDialog = new UserHabitsProgressDialogFragment();
                 habitDialog.show(((AppCompatActivity) context).getSupportFragmentManager(),
                         "user_progress" );
@@ -104,11 +142,23 @@ public class HabitsAdapter extends RecyclerView.Adapter<HabitsAdapter.MyViewHold
 
     }
 
+    /**
+     * Method to determine the number of items to be displayed for the RecyclerView.
+     * The size of the list of all HabitsModels is chosen.
+     *
+     * @return number of items to be displayed for the RecyclerView.
+     */
     @Override
     public int getItemCount() {
         return habitsModels.size();
     }
 
+    /**
+     * ViewHolder class for binding habit data to the corresponding views within a RecyclerView.
+     * This class holds references to the views for each habit item in the list. The views can be
+     * accessed and updated when the RecyclerView is scrolled or data is bound to the item.
+     *
+     */
     public static class MyViewHolder extends RecyclerView.ViewHolder{
         // grabbing views from rows
         ImageView habitImage;
@@ -117,6 +167,14 @@ public class HabitsAdapter extends RecyclerView.Adapter<HabitsAdapter.MyViewHold
         ImageView cancelButton;
         CardView habitCard;
 
+        /**
+         * Constructor for initializing the ViewHolder by binding the views in the provided itemView.
+         * This constructor finds the views within the item layout. These fields are later used
+         * to bind data to the views when the RecyclerView is scrolled and items are recycled.
+         *
+         * @param itemView The view representing an individual item in the RecyclerView. This view
+         *                 is passed to the ViewHolder to extract and bind the individual views
+         */
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
